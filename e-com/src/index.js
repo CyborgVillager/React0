86a79e5a7d6eadebe3134b18460c1e2170b99e99
CyborgVillager <almawijonathan@gmail.com>
2020-03-18 02:13:15 -0500

    v3 Page Setup
     Page Setup: updated menu category to navigate using with router. Quick template on using it.

--- e-com/src/index.js
+++ e-com/src/index.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import {BrowserRouter} from "react-router-dom";
 import App from './App';
 
 
 /* document.getElementById('root') gets the element from
 ReactDom / insert the App*/
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <BrowserRouter>
+    <App /></BrowserRouter>,
+    document.getElementById('root')
+);
